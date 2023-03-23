ff57fba817245c8208aff3399c7f3d496e585cc0
Hyojin <dlgywls1130@gmail.net>
2023-03-23 23:03:00 +0900

    contact, top scroll, size edit

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
+'use strict';
+
 
 
 // responsive menu
-(function($) {
-    $(function() {
+(function ($) {
+    $(function () {
         $('nav ul li > a:not(:only-child)').click(function (e) {
             $(this).siblings('.nav-dropdown').toggle();
             $('.nav-dropdown').not($(this).siblings()).hide();
@@ -22,3 +24,30 @@
 
 
 
+
+// Make home slowly fade to transparent as the window scrolls down
+const home = document.querySelector('.main');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+    home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+// Show "arrow up" button when scrolling down
+const arrowUp = document.querySelector('.scroll_btn');
+document.addEventListener('scroll', () => {
+    if (window.scrollY > homeHeight / 2) {
+        arrowUp.classList.add('visible');
+    } else {
+        arrowUp.classList.remove('visible');
+    }
+});
+
+// Handle click on the "arrow up" button
+arrowUp.addEventListener('click', () => {
+    scrollIntoView('#main');
+});
+
+function scrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({ behavior: 'smooth' });
+}
